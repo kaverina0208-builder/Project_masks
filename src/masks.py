76e6ec39e1f42a40256fd4c49dd90d40df72f7e0
Elena Kaverina <kaverina0208.elena@gmail.com>
2026-02-12 19:03:10 +0300

    add test_masks with tests for masks.py

--- src/masks.py
+++ src/masks.py
@@ -2,14 +2,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """The function returns the mask of the card number"""
     result = ""
-    if len(card_number) != 16 or card_number.isdigit() is False:
+    if len(card_number) != 16 or len(card_number) == "" or card_number.isdigit() is False:
         result = "Введен некорректный номер карты"
     else:
         result = f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}"
     return result
 
 
-# print(get_mask_card_number(card_number="1234567891231234"))
+print(get_mask_card_number(card_number="1234567891231234"))
 
 
 def get_mask_account(account_number: str) -> str:
@@ -22,4 +22,4 @@ def get_mask_account(account_number: str) -> str:
     return result
 
 
-# print(get_mask_account(account_number="12345678912345678912"))
+print(get_mask_account(account_number="12345678912345678912"))
