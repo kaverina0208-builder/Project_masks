b75e191121e4cd5fff82a028cf21bde6f1da4d0a
Elena Kaverina <kaverina0208.elena@gmail.com>
2026-02-15 11:48:09 +0300

    widget.py changes

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 import re
 
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(string_info: str) -> str:
